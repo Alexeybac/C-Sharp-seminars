72df6e7299b9fb55b0a4621aa85b9a6c54dc6021
Лёха <Alexbac@yandex.ru>
2023-03-11 16:50:22 +0300

    задачка 1.1

---
+++ 1_lesson/1.1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите первое число: ");
+int quatro = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число А: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a*a == quatro) Console.WriteLine($"Число А - является корнем числа {quatro}!");
+else Console.WriteLine($"Число А - НЕ является корнем числа {quatro}!");
+Console.WriteLine("Конец :) ");
