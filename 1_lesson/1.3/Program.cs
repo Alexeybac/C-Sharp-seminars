3c07c45d424683bec722420665a78a2f701fee78
Лёха <Alexbac@yandex.ru>
2023-03-11 17:54:31 +0300

    задачка 1.3

---
+++ 1_lesson/1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// Console.Write("Введите число:  ");
+// int N = int.Parse(Console.ReadLine());
+
+// for (int i = 0; i < N; i++)
+// {
+//         Console.Write(N*-1 + i);
+//         Console.Write(", ");
+// }
+// for (int i = 0; i < N+1; i++)
+// {
+//         Console.Write(i);
+//         Console.Write(", ");
+// }
+
+Console.Write("Введите число:  ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = -N; i <= N; i++)
+{
+        Console.Write(i);
+        Console.Write(", ");
+}
