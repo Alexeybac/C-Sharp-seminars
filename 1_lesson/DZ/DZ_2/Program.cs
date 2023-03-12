b379ea05b953d3c78d6c3ed80bf15a8da18ea960
Лёха <Alexbac@yandex.ru>
2023-03-12 16:27:14 +0300

    DZ_2

---
+++ 1_lesson/DZ/DZ_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает на вход три числа 
+// и выдает максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа, через ввод:   ");
+int n1= int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+
+int [] massiv = {n1, n2, n3};
+
+int count = massiv.Length;
+int i = 0;
+int max = massiv[0];
+while(i < count)
+{
+    if(massiv[i] > max) max = massiv[i];
+    i += 1;
+}
+Console.Write("Максимальное число из трех введенный будет:   ");
+Console.WriteLine(max);
