ef6bf0e3c8309fd75fbf5a271ebbdde4bd2ee48d
Лёха <Alexbac@yandex.ru>
2023-03-12 16:39:21 +0300

    DZ_3

---
+++ 1_lesson/DZ/DZ_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход принимает число
+// и выдает, является ли число четным
+// (делится ли оно на два без остатка).
+
+Console.Write("Введите число N:  ");
+int N = int.Parse(Console.ReadLine());
+
+int abs = N%2;
+
+if(abs != 0) 
+Console.Write("Число N нечетное");
+else
+Console.Write("Число N четное");
