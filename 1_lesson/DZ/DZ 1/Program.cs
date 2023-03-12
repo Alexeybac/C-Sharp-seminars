5e29bae5afb196c26f5d726a20a4f81c8773baea
Лёха <Alexbac@yandex.ru>
2023-03-12 15:12:35 +0300

    DZ_1

---
+++ 1_lesson/DZ/DZ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿// На вход принимает два числа и выдает, 
+// какое число больше, а какое меньше.
+
+Console.Write("Введите первое число N1:  ");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число N2:  ");
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1 < n2) 
+    Console.WriteLine($"Второе число {n2} больше первого {n1}");
+else
+    Console.Write($"Первое число {n1} больше второго {n2}");
