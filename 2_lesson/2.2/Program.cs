a95411f19f6587a281b6b58db65155ad5fca5bc7
Лёха <Alexbac@yandex.ru>
2023-03-12 20:19:46 +0300

    задачка 2.2

---
+++ 2_lesson/2.2/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите два числа построчно:  ");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1%n2 == 0)
+Console.WriteLine("кратно");
+else
+Console.WriteLine($"Не кратно, остаток {n1%n2}");
