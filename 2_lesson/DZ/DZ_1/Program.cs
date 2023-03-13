40d7687f6f9a27c3ae2eab50d939dca8b83307f5
Лёха <Alexbac@yandex.ru>
2023-03-13 18:35:07 +0300

    dz_1

---
+++ 2_lesson/DZ/DZ_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на вход трехзначное число,
+// на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трехзначное число N - ");
+int N = int.Parse(Console.ReadLine());
+
+if (N>99 && N<1000) 
+Console.WriteLine($"Вторая цифра числа N является: - {N/10%10}");
+else
+Console.WriteLine("!!!Ошибочка!!!");
+Console.Write("Введите ТРЕХ-значное число N - ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вторая цифра числа N является: - {n/10%10}");
