dd262bb03952612f2bc7aa9fbd6ba952c459e981
Лёха <Alexbac@yandex.ru>
2023-03-15 21:59:02 +0300

    3.2 Метод проверки на null

---
+++ 3_lesson/3.2/Проверка на Null.cs
@@ -0,0 +1,28 @@
+﻿
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+double Funck (int x1, int y1, int x2, int y2)
+{
+    double rez = Math.Sqrt(Math.Pow((x2 - x1),2) +  Math.Pow((y2 - y1),2));
+    return rez;
+}
+
+int X1 = ReadNumber("Введите Xa: ");
+int Y1 = ReadNumber("Введите Ya: ");
+int X2 = ReadNumber("Введите Xв: ");
+int Y2 = ReadNumber("Введите Yв: ");
+
+// Console.WriteLine("Введите координаты первой точки по строчно: ");
+// double X1 = double.Parse(Console.ReadLine());
+// double Y1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй точки по строчно: ");
+// double X2 = double.Parse(Console.ReadLine());
+// double Y2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Результат:  {Math.Round(Funck(X1,Y1,X2,Y2),2)}");
