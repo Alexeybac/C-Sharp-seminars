68ee96d6748fe0fb4189af3aea11dd0f6ba1d517
Лёха <Alexbac@yandex.ru>
2023-03-16 18:28:52 +0300

    3.3

---
+++ 3_lesson/3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿int NullOff(string text)
+{
+    Console.Write(text);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+void Mass (int x)
+{
+    Console.Write($"{x} => ");
+    for(int i = 1; i <= x; i++)
+    {
+        Console.Write($"{Math.Pow(i,2)}, ");
+    }
+}
+
+// int N = NullOff("Введите рандомную цифру: ");
+Mass(NullOff("Введите рандомную цифру: "));
