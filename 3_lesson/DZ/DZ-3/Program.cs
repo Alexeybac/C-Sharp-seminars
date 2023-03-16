82e8948351fb933fa52c75b9047d9a0d99ef54a5
Лёха <Alexbac@yandex.ru>
2023-03-16 21:27:17 +0300

    DZ_3

---
+++ 3_lesson/DZ/DZ-3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Принимает на вход число N,
+// выдает таблицу кубов от 1 до N.
+
+int Nulloff (string text)
+{
+    Console.Write(text);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+void Cube (int n)
+{
+    Console.Write($"Число {n} => ");
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n) Console.Write($"{i*i*i}. ");
+        else
+        Console.Write($"{i*i*i}, ");
+    }
+}
+
+Cube(Nulloff("Введите число :  "));
