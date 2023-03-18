27ea701175830307ffd385a5b164b1c1591633e2
Лёха <Alexbac@yandex.ru>
2023-03-18 22:36:17 +0300

    4.4

---
+++ 4_lesson/4.4/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Вариант 1.
+
+void Fillaray(int [] n)
+{
+    int length = n.Length;
+    for (int i = 0; i < length; i++)
+    {
+        n[i] = new Random().Next(0,2);
+    }
+}
+void Printaray(int[] m)
+{
+    int length = m.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write(m[i]);
+        Console.Write(", ");
+    }
+}
+
+Console.Write("Введите размер массива: ");
+int N = int.Parse(Console.ReadLine());
+int[] mass = new int[N];
+Fillaray(mass);
+Printaray(mass);
+
+
+// Вариант 2:
+
+
+void Massiv(int n)
+{
+int[] mass = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        mass[i] = new Random().Next(0,2);
+        Console.Write($"{mass[i]}, ");
+    }
+}
+
+Console.WriteLine();
+Console.Write("Введите размер массива: ");
+Massiv(int.Parse(Console.ReadLine()));
