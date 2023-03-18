1f15044e9a422ba955ada78c24c3e7b089c90abe
Лёха <Alexbac@yandex.ru>
2023-03-18 20:26:58 +0300

    4.0

---
+++ 4_lesson/4.0/Program.cs
@@ -0,0 +1,18 @@
+﻿int Nulloff(string text)
+{
+    Console.Write(text);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+int Summ( int x)
+{
+    if (x == 0)
+    return 0;
+    else
+    return x + Summ(x-1);
+    
+}
+
+// int N = Nulloff("Введите число:   ");
+Console.Write($"-------- {Summ(Nulloff("Введите число:   "))} --------");
