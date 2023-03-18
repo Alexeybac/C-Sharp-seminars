3175e24fccf9a9591215e7f54d0cdc92aafa8c85
Лёха <Alexbac@yandex.ru>
2023-03-18 21:38:57 +0300

    4.4

---
+++ 4_lesson/4.3/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+int Sum (int N)  // Рекурсия:
+{
+    if (N == 0) return 1;
+    return N * Sum(N-1);
+}
+
+Console.Write("Вводим число: ");
+Console.Write(Sum(int.Parse(Console.ReadLine())));
