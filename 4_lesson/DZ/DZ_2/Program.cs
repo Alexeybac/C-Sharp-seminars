245fa634d1d5bf043b43ae9ede0accec4b3d3d57
Лёха <Alexbac@yandex.ru>
2023-03-19 09:31:54 +0300

    DZ.2

---
+++ 4_lesson/DZ/DZ_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// На входе число,
+// на выходе сумма цифр в числе.
+
+int Nulof (string text)        // Исключение пустой строки
+{
+    Console.Write(text);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+int Sum (int N)                 // Решение через рекурсию! 
+{
+    if (N == 0) return N = 0;
+    else 
+    return N = N%10 + Sum(N/10);
+
+}
+
+int x = Nulof("Введите число:  ");
+Console.Write(Sum(x));
