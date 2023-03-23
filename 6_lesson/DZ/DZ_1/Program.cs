d3973d582c7011ccf26602db3bddd094d4d867d2
Лёха <Alexbac@yandex.ru>
2023-03-23 21:19:45 +0300

    DZ_1

--- 6_lesson/DZ/DZ_1/Program.cs
+++ 6_lesson/DZ/DZ_1/Program.cs
@@ -1,10 +1,9 @@
 ﻿// Пользователь вводит с клавиатуры М чисел,
 // посчитайте, сколько введено чисел больше нуля.
 
-Console.WriteLine("Введите кучу чисел: ");
+
 int Counts()
 {
-
     string txt;
     int count = 0;
     while (true)
@@ -15,13 +14,5 @@ int Counts()
     }
 }
 
+Console.WriteLine("Введите кучу чисел: ");
 Console.Write($"Количество полижительных чисел из введенных => {Counts()}");
-
-// var s = int.Parse(Console.ReadLine());
-// int count = 1;
-// while (s != null)
-// {
-//     s = int.Parse(Console.ReadLine());
-//     if (s > 0 ) count++;
-// }
-// Console.WriteLine(count);
