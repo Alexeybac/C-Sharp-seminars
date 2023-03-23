e0a52fd7ef3262b56b14c3c8c93121bafcbada2a
Лёха <Alexbac@yandex.ru>
2023-03-23 15:58:02 +0300

    6.3

---
+++ 6_lesson/6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿int NullOff (string text)
+{
+    Console.Write(text);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+int [] Fibonaccy (int N)
+{
+    int [] fib = new int[N];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < N; i++)
+    {
+        fib [i] = fib[i-1] + fib[i-2];
+    }
+    return fib;
+}
+void PrintFib (int [] aray)
+{
+    Console.Write("Расклад по Фибоначи: => ");
+    for (int i = 0; i < aray.Length; i++)
+    {
+        Console.Write($"{aray[i]} ");
+    }
+}
+
+int [] arr = Fibonaccy(NullOff("Введите число Фибоначи: "));
+PrintFib(arr);
