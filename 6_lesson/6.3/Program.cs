3c61e1205c483b6a37b65314a031df97e3f5e91d
Лёха <Alexbac@yandex.ru>
2023-03-23 16:17:55 +0300

    6.3

--- 6_lesson/6.3/Program.cs
+++ 6_lesson/6.3/Program.cs
@@ -1,22 +1,22 @@
-﻿int NullOff (string text)
+﻿int NullOff(string text)
 {
     Console.Write(text);
     var s = Console.ReadLine();
     int a = s == null ? 0 : int.Parse(s);
     return a;
 }
-int [] Fibonaccy (int N)
+int[] Fibonaccy(int N)  // == Массив ==
 {
-    int [] fib = new int[N];
+    int[] fib = new int[N];
     fib[0] = 0;
     fib[1] = 1;
     for (int i = 2; i < N; i++)
     {
-        fib [i] = fib[i-1] + fib[i-2];
+        fib[i] = fib[i - 1] + fib[i - 2];
     }
     return fib;
 }
-void PrintFib (int [] aray)
+void PrintFib(int[] aray)
 {
     Console.Write("Расклад по Фибоначи: => ");
     for (int i = 0; i < aray.Length; i++)
@@ -24,6 +24,19 @@ void PrintFib (int [] aray)
         Console.Write($"{aray[i]} ");
     }
 }
+void FibKorteg(int N)   // == Кортеж ==
+{
+    Console.Write("Расклад КОРТЕЖЕМ по Фибоначи: => ");
+    int a = 0, b = 1;
+    for (int i = 0; i < N; i++)
+    {
+        Console.Write($"{a} ");
+        (a, b) = (b, a + b);
+    }
+}
 
-int [] arr = Fibonaccy(NullOff("Введите число Фибоначи: "));
-PrintFib(arr);
+int num = NullOff("Введите число Фибоначи: ");
+
+PrintFib(Fibonaccy(num));
+Console.WriteLine();
+FibKorteg(num);
