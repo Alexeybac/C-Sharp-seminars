fbffd7d1978b4a50f3b4d15d2a5b38ba62d1d764
Лёха <Alexbac@yandex.ru>
2023-03-23 15:32:23 +0300

    5.2

---
+++ 6_lesson/6.1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int NullOff(string txt)
+{
+    Console.WriteLine(txt);
+    var s = Console.ReadLine();
+    int x = s == null ? 0 : int.Parse(s);
+    return x;
+}
+
+string Triangl()
+{
+    Console.WriteLine("Введите длины сторон построчно.");
+    int a = NullOff("Введите сторону А: ");
+    int b = NullOff("Введите сторону Б: ");
+    int c = NullOff("Введите сторону С: ");
+    if (a + b > c && a + c > b && c + b > a) return "Да!";
+    return "Нет!";
+}
+
+// Console.WriteLine("Введите длины сторон построчно.");
+// int x = NullOff("Введите сторону А: ");
+// int y = NullOff("Введите сторону Б: ");
+// int z = NullOff("Введите сторону С: ");
+
+Console.WriteLine(Triangl());
