16d5a7098fe450e2e1fcd01e20f24558544b0f5d
Лёха <Alexbac@yandex.ru>
2023-03-27 17:29:05 +0300

    DZ_2

---
+++ 9_lesson/DZ/DZ_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N, 
+// выводит сумму натуральных чисел в промежутке от M до N,
+// с помощью рекурсии.
+
+Console.Clear();
+int NullOff(string txt)
+{
+    Console.Write(txt);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+int SteepSum(int m, int n)
+{
+    if (m > n) return 0;
+    return m + SteepSum(m + 1, n);
+}
+
+
+int M = NullOff("Введите первое число:  ");
+int N = NullOff("Введите второе число:  ");
+Console.WriteLine(SteepSum(M, N));
+
+Console.ReadKey();
