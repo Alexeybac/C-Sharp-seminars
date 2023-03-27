1ba3c5a9ab7825331f2e36523784a5fdea8449ae
Лёха <Alexbac@yandex.ru>
2023-03-27 16:44:20 +0300

    9

--- 9_lesson/9.1/Program.cs
+++ 9_lesson/9.1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Рекурсия выводит от 1 до N.
+
+Console.Clear();
+int NullOff (string txt)
+{
+    Console.Write(txt);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+void Nums (int N)
+{
+    if (N < 1) return;
+    Nums(N-1);
+    Console.Write($"{N}, ");
+    
+}
+
+int n = NullOff("Введите число N:  ");
+Nums(n);
---
+++ 9_lesson/9.3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Рекурсия выводит сумму цифр числа N.
+
+Console.Clear();
+int NullOff(string txt)
+{
+    Console.Write(txt);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+int SumNum (int N)
+{
+    if (N%10 == 0) return 0;
+    return N%10 + SumNum(N/10);
+}
+
+int n = NullOff("Введите число M:  ");
+Console.Write(SumNum(n));
